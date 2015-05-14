05c4ec530d02986fea317c3f936f344fa43cb518
James Hagerman <james.hagerman@gmail.com>
2015-05-14 01:52:12 -0700

    Getting the UART interrupt coded. I always forget about how useful the compiler docs are...

--- L1DemoJamisNemo.X/main.c
+++ L1DemoJamisNemo.X/main.c
@@ -135,11 +135,51 @@ void config_graphics(void) {
 	__delay_ms(1);
 }
 
+void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt( void ) {
+    // Clear interrupt flag
+    IFS0bits.U1RXIF = 0;
+
+    //Check for UART receive overrun
+    if(U1STAbits.OERR == 1) {
+        U1STAbits.OERR = 0;
+    } else {
+        if(U1STAbits.FERR == 0) {
+
+            // Read out the UART FIFO
+            while(U1STAbits.URXDA == 1) {
+                // grab from U1RXREG;
+            }
+        } else {
+            // UART framming error...
+            // grab from U1RXREG;
+        }
+    }
+}
 void config_uart(void) {
     OSCCONbits.IOLOCK = 0; // unlock the peripheral Control Register Lock
     RPINR18bits.U1RXR = 7; // Map UART1 RX peripheral to RP7
     RPOR3bits.RP6R = 3; // Map RP6 pin to UART1 TX
     OSCCONbits.IOLOCK = 1; // relock the peripheral Control Register Lock
+
+
+    /* ripped from some other code:
+#define BAUDRATE2       31250UL
+#define BRG_DIV2        4
+#define BRGH2           1
+     */
+    U1BRG = (((FCY)/(4 * 9600UL)) - 1);
+    U1MODE = 0;
+    U1MODEbits.BRGH = 1;
+    U1STA = 0;
+    U1MODEbits.UARTEN = 1;
+    U1STAbits.UTXEN = 1;
+    IFS0bits.U1RXIF = 0;
+
+    
+
+    // Turn interrupt for UART receive on, with highest priority
+    IEC0bits.U1RXIE = 1;
+    IPC7 |= 0x0700;
 }
 
 void config_chr(void) {
